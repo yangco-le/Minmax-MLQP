699baba50df58bdf117d1eccb809e29728eac43f
yangco-le <yanglily@sjtu.edu.cn>
2022-03-24 15:23:33 +0800

    feat: model design

--- utils.py
+++ utils.py
@@ -4,3 +4,8 @@ import random
 def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
+
+def sigmoid(x, derive=False):
+    if derive:
+        return x * (1 - x)
+    return 1 / (1 + np.exp(-x))
