084736ad48e79a6e03476415ed191d317a5c64bc
yangco-le <yanglily@sjtu.edu.cn>
2022-03-25 19:14:05 +0800

    feat: two spiral problem and minmax

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import os
+import time
 import numpy as np
 from absl import flags, app
 from model import MLQP
-from utils import dataloader, set_seed
+from utils import *
 from tqdm import trange
 import pandas as pd
 
 FLAGS = flags.FLAGS
-# flags.DEFINE_enum('mode', 'train', ['train', 'test'], "Execution mode")
-flags.DEFINE_float('lr', 1e-2, "Learning rate")
+flags.DEFINE_enum('mode', 'minmax', ['vanilla', 'minmax'], "Execution mode")
+flags.DEFINE_float('lr', 1e-1, "Learning rate")
 flags.DEFINE_integer('num_hidden', 32, "Number of hidden neurons")
-flags.DEFINE_integer('num_epoch', 2000, "Number of epoches")
+flags.DEFINE_integer('num_epoch', 10000, "Number of epoches")
 flags.DEFINE_integer('seed', 0, "Random seed")
-flags.DEFINE_string('name', "mlqp", "Experiment name")
+flags.DEFINE_enum('partition', "radius", ["random", "yaxis", "radius"], "Partition mode")
+flags.DEFINE_string('name', "minmax_lr1e-1", "Experiment name")
 
 def train():
     train_data = dataloader("train")
@@ -20,7 +22,12 @@ def train():
 
     mlqp = MLQP(1, 2, FLAGS.num_hidden, 1)
 
-    train_loss, train_acc, test_loss, test_acc = [], [], [], []
+    log_dir = os.path.join("logs", FLAGS.name)
+    os.makedirs(os.path.join(log_dir, "visualize"), exist_ok=True)
+
+    time0 = time.time()
+    r_time = 0
+    train_loss, train_acc, test_loss, test_acc, li_time = [], [], [], [], []
     with trange(1, FLAGS.num_epoch + 1, desc='Training', ncols=0) as pbar:
         for epoch in pbar:
             #train
@@ -31,34 +38,148 @@ def train():
                 mlqp.step(FLAGS.lr)
                 preds.append(pred)
             preds = np.squeeze(np.array(preds))
-            loss = 0.5 * (y - preds) ** 2
-            acc = ((preds > 0.5) == train_data[2]).mean()
+            loss = (0.5 * (train_data[:, 1] - preds) ** 2).sum()
+            acc = ((preds > 0.5) == train_data[:, 1]).mean()
             pbar.set_postfix(loss='%.4f' % loss, acc='%.4f' % acc)
-            train_loss.append(loss)
-            train_acc.append(acc)
 
             # test
-            if epoch % 50 == 0:
+            if epoch % 10 == 0:
+                # training log 
+                train_loss.append(loss)
+                train_acc.append(acc)
+
+                # testing log
                 preds = []
-                for cor, y in zip(test_data):
+                for cor, y in test_data:
                     pred = mlqp.forward(cor)
                     preds.append(pred)
                 preds = np.squeeze(np.array(preds))
-                loss = 0.5 * (y - preds) ** 2
-                acc = ((preds > 0.5) == train_data[2]).mean()
+                loss_test = (0.5 * (test_data[:, 1] - preds) ** 2).sum()
+                acc_test = ((preds > 0.5) == test_data[:, 1]).mean()
                 pbar.set_postfix(loss='%.4f' % loss, acc='%.4f' % acc)
-                test_loss.append(loss)
-                test_acc.append(acc)
+                test_loss.append(loss_test)
+                test_acc.append(acc_test)
+
+                # time log
+                li_time.append(time.time() - time0 - r_time)
+            
+            if epoch % 1000 == 0:
+                r_time0 = time.time()
+                # plot decision boundary
+                plot_decision_boundary(lambda x: mlqp.forward(x), np.stack(train_data[:, 0]).T, train_data[:, 1], 
+                                       os.path.join(log_dir, "visualize", "epoch_{}".format(epoch)))
+                r_time += time.time() - r_time0
+
+    dataframe = pd.DataFrame({'time': li_time, 'train_loss': train_loss, 'train_acc': train_acc, 'test_loss': test_loss, 
+                              'test_acc': test_acc})
+    dataframe.to_csv(os.path.join(log_dir, "log.csv"), sep=',')
+
+def train_minmax():
+    train_data = dataloader("train")
+    one_set, zero_set = partition_data(dataloader("train"), FLAGS.partition)
+    train_data_tmp = []
+    for i in range(2):
+        for j in range(2):
+            train_data_tmp.append(one_set[i] + zero_set[j])
+    test_data = dataloader("test")
+
+    models = []
+    for i in range(4):
+        models.append(MLQP(1, 2, FLAGS.num_hidden, 1))
 
     log_dir = os.path.join("logs", FLAGS.name)
-    os.makedirs(os.path.join(log_dir, "visualize"))
-    dataframe = pd.DataFrame({'train_loss': train_loss, 'train_acc': train_acc, 'test_loss': train_loss, 'test_acc': train_acc})
+    os.makedirs(os.path.join(log_dir, "visualize"), exist_ok=True)
+
+    time0 = time.time()
+    r_time = 0
+    train_loss, train_acc, test_loss, test_acc, li_time = [], [], [], [], []
+    with trange(1, FLAGS.num_epoch + 1, desc='Training', ncols=0) as pbar:
+        for epoch in pbar:
+            # train
+            for i in range(2):
+                for j in range(2):
+                    tmp_data = train_data_tmp[2 * i + j]
+                    for cor, y in tmp_data:
+                        pred = models[2 * i + j].forward(cor)
+                        models[2 * i + j].backward(y)
+                        models[2 * i + j].step(FLAGS.lr)
+            
+            preds = [[], [], [], []]
+            for i in range(4):
+                for x, y in train_data:
+                    preds[i].append(models[i].forward(x)[0][0])
+            preds = np.array(preds)
+            preds = np.max((np.min((preds[0], preds[1]), 0), np.min((preds[2], preds[3]), 0)), 0)
+            loss = (0.5 * (train_data[:, 1] - preds) ** 2).sum()
+            acc = ((preds > 0.5) == train_data[:, 1]).mean()
+            pbar.set_postfix(loss='%.4f' % loss, acc='%.4f' % acc)
+
+            # test
+            if epoch % 10 == 0:
+                # training log 
+                train_loss.append(loss)
+                train_acc.append(acc)
+
+                # testing log
+                preds = [[], [], [], []]
+                for i in range(4):
+                    for x, y in test_data:
+                        preds[i].append(models[i].forward(x)[0][0])
+                preds = np.array(preds)
+                preds = np.max((np.min((preds[0], preds[1]), 0), np.min((preds[2], preds[3]), 0)), 0)
+                loss_test = (0.5 * (test_data[:, 1] - preds) ** 2).sum()
+                acc_test = ((preds > 0.5) == test_data[:, 1]).mean()
+                pbar.set_postfix(loss='%.4f' % loss, acc='%.4f' % acc)
+                test_loss.append(loss_test)
+                test_acc.append(acc_test)
+
+                # time log
+                li_time.append(time.time() - time0 - r_time)
+            
+            if epoch % 1000 == 0:
+                r_time0 = time.time()
+                # plot decision boundary
+                plot_decision_boundary(lambda x: models[0].forward(x), 
+                                       np.stack(np.array(train_data_tmp[0])[:, 0]).T, 
+                                       np.array(train_data_tmp[0])[:, 1], 
+                                       os.path.join(log_dir, "visualize", "epoch_{}_model_1".format(epoch)))
+                plot_decision_boundary(lambda x: models[1].forward(x), 
+                                       np.stack(np.array(train_data_tmp[1])[:, 0]).T, 
+                                       np.array(train_data_tmp[1])[:, 1], 
+                                       os.path.join(log_dir, "visualize", "epoch_{}_model_2".format(epoch)))
+                plot_decision_boundary(lambda x: models[2].forward(x), 
+                                       np.stack(np.array(train_data_tmp[2])[:, 0]).T, 
+                                       np.array(train_data_tmp[2])[:, 1], 
+                                       os.path.join(log_dir, "visualize", "epoch_{}_model_3".format(epoch)))
+                plot_decision_boundary(lambda x: models[3].forward(x), 
+                                       np.stack(np.array(train_data_tmp[3])[:, 0]).T,
+                                       np.array(train_data_tmp[3])[:, 1], 
+                                       os.path.join(log_dir, "visualize", "epoch_{}_model_4".format(epoch)))
+                plot_decision_boundary(lambda x: min(models[0].forward(x), models[1].forward(x)), 
+                                       np.stack(np.array(train_data_tmp[0] + train_data_tmp[1])[:, 0]).T, 
+                                       np.array(train_data_tmp[0] + train_data_tmp[1])[:, 1], 
+                                       os.path.join(log_dir, "visualize", "epoch_{}_model_12".format(epoch)))
+                plot_decision_boundary(lambda x: min(models[2].forward(x), models[3].forward(x)), 
+                                       np.stack(np.array(train_data_tmp[2] + train_data_tmp[3])[:, 0]).T, 
+                                       np.array(train_data_tmp[2] + train_data_tmp[3])[:, 1], 
+                                       os.path.join(log_dir, "visualize", "epoch_{}_model_34".format(epoch)))
+                plot_decision_boundary(lambda x: max(min(models[0].forward(x), models[1].forward(x)), min(models[2].forward(x), models[3].forward(x))), 
+                                       np.stack(train_data[:, 0]).T, 
+                                       train_data[:, 1], 
+                                       os.path.join(log_dir, "visualize", "epoch_{}_model_1234".format(epoch)))
+                r_time += time.time() - r_time0
+
+    dataframe = pd.DataFrame({'time': li_time, 'train_loss': train_loss, 'train_acc': train_acc, 'test_loss': test_loss, 
+                              'test_acc': test_acc})
     dataframe.to_csv(os.path.join(log_dir, "log.csv"), sep=',')
 
 
 def main(argv):
     set_seed(FLAGS.seed)
-    train()
+    if FLAGS.mode == "vanilla":
+        train()
+    elif FLAGS.mode == "minmax":
+        train_minmax()
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import os
+import matplotlib.pyplot as plt
 
 def set_seed(seed):
     random.seed(seed)
@@ -18,3 +19,61 @@ def dataloader(mode):
         res.append([np.array([float(line.split()[0]), float(line.split()[1])]), int(line.split()[2])])
     file.close()
     return np.array(res)
+
+def plot_decision_boundary(model, X, y, filename):
+    # Set min and max values and give it some padding
+    x_min, x_max = -7, 7
+    y_min, y_max = -7, 7
+    h = 0.05
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    data = np.c_[xx.ravel(), yy.ravel()]
+    Z = []
+    for i in data:
+        tmp = model(i)
+        Z.append(tmp)
+    Z = np.array(Z)
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
+    plt.savefig(filename)
+
+def partition_data(data, mode):
+    x, y = data[:, 0], data[:, 1]
+    x_one, y_one = x[np.where(y == 1)], y[np.where(y == 1)]
+    x_zero, y_zero = x[np.where(y == 0)], y[np.where(y == 0)]
+    one_set, zero_set = [[], []], [[], []]
+
+    if mode == 'random':
+        for i, j in enumerate(np.random.choice(2, len(x_one))):
+            one_set[j].append((x_one[i], y_one[i]))
+        for i, j in enumerate(np.random.choice(2, len(x_zero))):
+            zero_set[j].append((x_zero[i], y_zero[i]))
+    elif mode == 'yaxis':
+        for i in range(len(x_one)):
+            if x_one[i][0] >= -0.3:
+                one_set[1].append((x_one[i], y_one[i]))
+            elif x_one[i][0] <= 0.3:
+                one_set[0].append((x_one[i], y_one[i]))
+        for i in range(len(x_zero)):
+            if x_zero[i][0] >= -0.3:
+                zero_set[1].append((x_zero[i], y_zero[i]))
+            elif x_zero[i][0] <= 0.3:
+                zero_set[0].append((x_zero[i], y_zero[i]))
+    elif mode == 'radius':
+        for i in range(len(x_one)):
+            if x_one[i][0] ** 2 + x_one[i][1] ** 2 >= 8:
+                one_set[1].append((x_one[i], y_one[i]))
+            elif x_one[i][0] ** 2 + x_one[i][1] ** 2 <= 10:
+                one_set[0].append((x_one[i], y_one[i]))
+        for i in range(len(x_zero)):
+            if x_zero[i][0] ** 2 + x_zero[i][1] ** 2 >= 8:
+                zero_set[1].append((x_zero[i], y_zero[i]))
+            elif x_zero[i][0] ** 2 + x_zero[i][1] ** 2 <= 10:
+                zero_set[0].append((x_zero[i], y_zero[i]))
+
+    return one_set, zero_set
